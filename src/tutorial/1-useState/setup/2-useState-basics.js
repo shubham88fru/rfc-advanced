c20b06981f030db66ef8d0361ae26b35764c3e7c
Shubham Singh <shubham88fru@yahoo.com>
2021-12-16 16:37:24 +0530

    use useState to update the state

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  /**
+   * 1) useState needs a initial value, else initial value
+   * is undefined.
+   *
+   * 2) returns an array - first element is state and second
+   * element is a handler used to update the state.
+   *
+   * 3) Everytime state updates, component re-renders
+   */
+  const [value, setValue] = useState('INITIAL TITLE');
+
+  const handleClick = (e) => {
+    setValue('Hello world');
+  };
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={handleClick}>
+        CHANGE TITLE
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
