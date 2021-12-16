1786e5cff3f03ef540c675d7a7318eb739bac5a7
Shubham Singh <shubham88fru@yahoo.com>
2021-12-16 16:27:17 +0530

    change component variable like a noob

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'random title';
+  const handleClick = () => {
+    title = 'Hello People'; // title in theory changes but not updated on UI
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        CHANGE TITLE
+      </button>
+    </>
+  );
 };
 
 export default ErrorExample;
