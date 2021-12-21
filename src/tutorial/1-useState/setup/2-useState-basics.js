6cb67c642650d32b652613f0075f28d74cc66c16
shubham88fru <shubham88fru@yahoo.com>
2021-12-21 11:48:52 +0530

    hooks rule of the road

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
-
+/**
+ * REACT HOOKS - Rules of Road.
+ * 1) Hook names usually start with `use`
+ * 2) Component inside which hook is being used needs to be uppercase.
+ * 3) Hook should be invoked inside the FC component body. Can't call hook in
+ * global module context.
+ * 4) Can't call hooks conditionally.
+ */
 const UseStateBasics = () => {
   /**
    * 1) useState needs a initial value, else initial value
